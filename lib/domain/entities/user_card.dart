4ef59379b570d90499daded51144f60a29fd2928
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2022-06-22 12:32:07 +0200

    removed api key

--- lib/domain/entities/user_card.dart
+++ lib/domain/entities/user_card.dart
@@ -4,24 +4,24 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import '../../constants.dart';
 import 'date_time_converter.dart';
 
-part 'user.freezed.dart';
+part 'user_card.freezed.dart';
 
-part 'user.g.dart';
+part 'user_card.g.dart';
 
 @freezed
-class User with _$User {
-  const factory User({
+class UserCard with _$UserCard {
+  const factory UserCard({
     required String id,
     required String name,
     required String surname,
     required String cf,
     @MyDateTimeConverter() DateTime? addedOn,
-  }) = _User;
+  }) = _UserCard;
 
-  factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+  factory UserCard.fromJson(Map<String, Object?> json) => _$UserCardFromJson(json);
 }
 
-extension UserX on User {
+extension UserCardX on UserCard {
   String qrCode(String sessionId) =>
       '$qrCodePrefix%$id%$name%$surname%$cf%$sessionId%$qrCodePrefix';
 
