46f0f7f3cbb4c83bf6ff0622d66a5034ff82883c
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2023-07-13 09:25:06 +0200

    Fix cors on scan api

--- functions/src/scan_processing.ts
+++ functions/src/scan_processing.ts
@@ -5,6 +5,7 @@ dotenv.config();
 const db = admin.firestore();
 //const cors = require("cors")({ origin: true });
 import * as firestore from "@google-cloud/firestore";
+const cors = require("cors")({ origin: true });
 import {
   //userDocRef,
   //privateUserDocRef,
@@ -16,203 +17,148 @@ export const scan = functions
   .region("europe-west3")
   .https.onRequest(
     async (request: functions.https.Request, response: functions.Response) => {
-      console.log(request.body);
+      cors(request, response, async () => {
+        console.log(request.body);
 
-      if (request.method !== "POST") {
-        response.status(403).send("Forbidden!");
-        return;
-      }
-
-      const data = request.body;
-      const eventId = data.eventId;
-      const userId = data.userId;
-      const groupId = data.groupId;
-      const privateId = data.privateId;
-      const hasPrivateInfo = data.hasPrivateInfo ?? privateId != null;
-      const userCardProviderId = data.userCardProviderId;
-      const eventProviderId = data.providerId;
-      const isSingleAccessType = data.isSingleAccessType;
-      const scanMode = data.scanMode;
-      const isGroup = data.isGroup;
-      const groupLeaderId = data.groupLeaderId;
-      const isAnonymous = data.isAnonymous;
-      const groupName = data.groupName;
-      const name = data.name;
-      const surname = data.surname;
-      const cf = data.cf;
-      const fromExternalOrganization = data.fromExternalOrganization;
-      const email = data.email;
-      const groupCount = data.groupCount;
-      const averageAge = data.averageAge;
-      const manPercentage = data.manPercentage;
-      const womanPercentage = data.womanPercentage;
-      const timestamp = new Date(data.timestamp);
-      const firestoreTimestamp = firestore.Timestamp.fromDate(timestamp);
-
-      if (eventProviderId == null || userCardProviderId == null) {
-        response.status(400).send("invalid providerId or userCardProviderId");
-        return;
-      }
-
-      if (isAnonymous && privateId == null) {
-        response.status(400).send("invalid privateId");
-        return;
-      }
-
-      if (!isGroup && (userId == null || userId == "")) {
-        response.status(400).send("invalid userId");
-        return;
-      }
-
-      if (isGroup && (groupLeaderId == null || groupId == null)) {
-        response.status(400).send("invalid groupLeaderId");
-        return;
-      }
-
-      const eventRef = eventDocRef(eventProviderId, eventId);
-      const eventDoc = await eventRef.get();
-      const event = eventDoc.data();
-
-      if (event == null) {
-        response.status(400).send("event is null, " + eventRef.path);
-        return;
-      }
-
-      const id = isGroup ? groupId : userId;
-      const userSubEventDocRef = sessionDocRef(
-        eventProviderId,
-        eventId,
-        event.activeSessionId!
-      )
-        .collection("users")
-        .doc(isAnonymous ? privateId : id);
-      console.log(userSubEventDocRef.path);
-      const userDoc = await userSubEventDocRef.get();
-      const userData = userDoc.data();
-      const userSubEventExists = userDoc.exists && userData != null;
-      console.log(
-        isAnonymous
-          ? privateId
-          : id +
-              " exists in the event collection? :  " +
-              userSubEventExists +
-              " "
-      );
-
-      // Check out
-      if (!isSingleAccessType && scanMode == "checkOut") {
-        // se l utente esiste vuol dire che ha fatto il check in
-        if (userSubEventExists) {
-          // Se non è stato fatto ancora il check out aggiungo la data di check out
-          if (userData.checkOutAt == null) {
-            await userSubEventDocRef.update({ checkOutAt: firestoreTimestamp });
-            response.status(200).send({
-              type: "check_out",
-              status: "check_out_completed",
-            });
-            return;
-          }
-        } else {
-          response.status(200).send({
-            type: "check_out",
-            status: "missing_check_in",
-          });
+        if (request.method !== "POST") {
+          response.status(403).send("Forbidden!");
           return;
         }
-      }
 
-      // Check in
-      if (userSubEventExists) {
-        console.log(eventProviderId);
-        console.log(eventId);
-        console.log(id);
-        const ref = await eventDocRef(eventProviderId, eventId)
+        const data = request.body;
+        const eventId = data.eventId;
+        const userId = data.userId;
+        const groupId = data.groupId;
+        const privateId = data.privateId;
+        const hasPrivateInfo = data.hasPrivateInfo ?? privateId != null;
+        const userCardProviderId = data.userCardProviderId;
+        const eventProviderId = data.providerId;
+        const isSingleAccessType = data.isSingleAccessType;
+        const scanMode = data.scanMode;
+        const isGroup = data.isGroup;
+        const groupLeaderId = data.groupLeaderId;
+        const isAnonymous = data.isAnonymous;
+        const groupName = data.groupName;
+        const name = data.name;
+        const surname = data.surname;
+        const cf = data.cf;
+        const fromExternalOrganization = data.fromExternalOrganization;
+        const email = data.email;
+        const groupCount = data.groupCount;
+        const averageAge = data.averageAge;
+        const manPercentage = data.manPercentage;
+        const womanPercentage = data.womanPercentage;
+        const timestamp = new Date(data.timestamp);
+        const firestoreTimestamp = firestore.Timestamp.fromDate(timestamp);
+
+        if (eventProviderId == null || userCardProviderId == null) {
+          response.status(400).send("invalid providerId or userCardProviderId");
+          return;
+        }
+
+        if (isAnonymous && privateId == null) {
+          response.status(400).send("invalid privateId");
+          return;
+        }
+
+        if (!isGroup && (userId == null || userId == "")) {
+          response.status(400).send("invalid userId");
+          return;
+        }
+
+        if (isGroup && (groupLeaderId == null || groupId == null)) {
+          response.status(400).send("invalid groupLeaderId");
+          return;
+        }
+
+        const eventRef = eventDocRef(eventProviderId, eventId);
+        const eventDoc = await eventRef.get();
+        const event = eventDoc.data();
+
+        if (event == null) {
+          response.status(400).send("event is null, " + eventRef.path);
+          return;
+        }
+
+        const id = isGroup ? groupId : userId;
+        const userSubEventDocRef = sessionDocRef(
+          eventProviderId,
+          eventId,
+          event.activeSessionId!
+        )
           .collection("users")
           .doc(isAnonymous ? privateId : id);
-
-        await ref.update({
-          participationCount: firestore.FieldValue.increment(1),
-        });
-        response.status(200).send({
-          type: "check_in",
-          status: "participation_count_incremented",
-        });
-        return;
-      } else {
-        // Se è un gruppo controllo che il rappresentante abbia fatto checkin
-        if (isGroup) {
-          const groupLeaderDoc = await eventDocRef(eventProviderId, eventId)
-            .collection("users")
-            .doc(groupLeaderId)
-            .get();
-          if (!groupLeaderDoc.exists) {
-            response.status(200).send({
-              type: "check_in",
-              status: "leader_group_not_exist",
-            });
-            return;
-          }
-        }
-
-        const json: Record<string, any> = {
-          isGroup: isGroup,
-          id: userId,
-          name: name,
-          surname: surname,
-          email: email,
-          cf: cf,
-          fromExternalOrganization: fromExternalOrganization,
-          privateId: privateId,
-          groupName: groupName,
-          groupCount: groupCount,
-          averageAge: averageAge,
-          manPercentage: manPercentage,
-          womanPercentage: womanPercentage,
-          isAnonymous: isAnonymous,
-          providerId: userCardProviderId,
-          hasPrivateInfo: hasPrivateInfo,
-        };
-
-        if (isSingleAccessType && scanMode == "checkOut") {
-          //eventUser = eventUser.copyWith(checkOutAt: DateTime.now());
-          json["checkOutAt"] = firestoreTimestamp;
-        } else if (!isSingleAccessType && scanMode == "checkIn") {
-          //eventUser = eventUser.copyWith(checkInAt: DateTime.now());
-          json["checkInAt"] = firestoreTimestamp;
-        }
-
-        const batch = db.batch();
-
-        // Aggiungo l utente nella lista utenti dell evento
-        batch.set(userSubEventDocRef, json);
-
-        if (!isGroup && privateId != null) {
-          console.log(
-            "Salvo il mio private id nella collezione privateUsers della sessione"
-          );
-          batch.set(
-            sessionDocRef(eventProviderId, eventId, event.activeSessionId!)
-              .collection("privateUsers")
-              .doc(privateId),
-            {
-              id: privateId,
-            }
-          );
-        }
-
-        const globalUserRef = eventDocRef(eventProviderId, eventId)
-          .collection("users")
-          .doc(isAnonymous ? privateId : id);
-        const globalUserDoc = await globalUserRef.get();
+        console.log(userSubEventDocRef.path);
+        const userDoc = await userSubEventDocRef.get();
+        const userData = userDoc.data();
+        const userSubEventExists = userDoc.exists && userData != null;
         console.log(
           isAnonymous
             ? privateId
-            : id + " esiste nella collezione globale? " + globalUserDoc.exists
+            : id +
+                " exists in the event collection? :  " +
+                userSubEventExists +
+                " "
         );
 
-        if (!globalUserDoc.exists) {
-          console.log("Salvo nuovo utente nella collezione globale");
-          batch.set(globalUserRef, {
+        // Check out
+        if (!isSingleAccessType && scanMode == "checkOut") {
+          // se l utente esiste vuol dire che ha fatto il check in
+          if (userSubEventExists) {
+            // Se non è stato fatto ancora il check out aggiungo la data di check out
+            if (userData.checkOutAt == null) {
+              await userSubEventDocRef.update({
+                checkOutAt: firestoreTimestamp,
+              });
+              response.status(200).send({
+                type: "check_out",
+                status: "check_out_completed",
+              });
+              return;
+            }
+          } else {
+            response.status(200).send({
+              type: "check_out",
+              status: "missing_check_in",
+            });
+            return;
+          }
+        }
+
+        // Check in
+        if (userSubEventExists) {
+          console.log(eventProviderId);
+          console.log(eventId);
+          console.log(id);
+          const ref = await eventDocRef(eventProviderId, eventId)
+            .collection("users")
+            .doc(isAnonymous ? privateId : id);
+
+          await ref.update({
+            participationCount: firestore.FieldValue.increment(1),
+          });
+          response.status(200).send({
+            type: "check_in",
+            status: "participation_count_incremented",
+          });
+          return;
+        } else {
+          // Se è un gruppo controllo che il rappresentante abbia fatto checkin
+          if (isGroup) {
+            const groupLeaderDoc = await eventDocRef(eventProviderId, eventId)
+              .collection("users")
+              .doc(groupLeaderId)
+              .get();
+            if (!groupLeaderDoc.exists) {
+              response.status(200).send({
+                type: "check_in",
+                status: "leader_group_not_exist",
+              });
+              return;
+            }
+          }
+
+          const json: Record<string, any> = {
             isGroup: isGroup,
             id: userId,
             name: name,
@@ -220,6 +166,7 @@ export const scan = functions
             email: email,
             cf: cf,
             fromExternalOrganization: fromExternalOrganization,
+            privateId: privateId,
             groupName: groupName,
             groupCount: groupCount,
             averageAge: averageAge,
@@ -227,16 +174,28 @@ export const scan = functions
             womanPercentage: womanPercentage,
             isAnonymous: isAnonymous,
             providerId: userCardProviderId,
-            participationCount: 1,
             hasPrivateInfo: hasPrivateInfo,
-          });
+          };
+
+          if (isSingleAccessType && scanMode == "checkOut") {
+            //eventUser = eventUser.copyWith(checkOutAt: DateTime.now());
+            json["checkOutAt"] = firestoreTimestamp;
+          } else if (!isSingleAccessType && scanMode == "checkIn") {
+            //eventUser = eventUser.copyWith(checkInAt: DateTime.now());
+            json["checkInAt"] = firestoreTimestamp;
+          }
+
+          const batch = db.batch();
+
+          // Aggiungo l utente nella lista utenti dell evento
+          batch.set(userSubEventDocRef, json);
 
           if (!isGroup && privateId != null) {
             console.log(
-              "Salvo il mio private id nella collezione privateUsers"
+              "Salvo il mio private id nella collezione privateUsers della sessione"
             );
             batch.set(
-              eventDocRef(eventProviderId, eventId)
+              sessionDocRef(eventProviderId, eventId, event.activeSessionId!)
                 .collection("privateUsers")
                 .doc(privateId),
               {
@@ -244,14 +203,60 @@ export const scan = functions
               }
             );
           }
+
+          const globalUserRef = eventDocRef(eventProviderId, eventId)
+            .collection("users")
+            .doc(isAnonymous ? privateId : id);
+          const globalUserDoc = await globalUserRef.get();
+          console.log(
+            isAnonymous
+              ? privateId
+              : id + " esiste nella collezione globale? " + globalUserDoc.exists
+          );
+
+          if (!globalUserDoc.exists) {
+            console.log("Salvo nuovo utente nella collezione globale");
+            batch.set(globalUserRef, {
+              isGroup: isGroup,
+              id: userId,
+              name: name,
+              surname: surname,
+              email: email,
+              cf: cf,
+              fromExternalOrganization: fromExternalOrganization,
+              groupName: groupName,
+              groupCount: groupCount,
+              averageAge: averageAge,
+              manPercentage: manPercentage,
+              womanPercentage: womanPercentage,
+              isAnonymous: isAnonymous,
+              providerId: userCardProviderId,
+              participationCount: 1,
+              hasPrivateInfo: hasPrivateInfo,
+            });
+
+            if (!isGroup && privateId != null) {
+              console.log(
+                "Salvo il mio private id nella collezione privateUsers"
+              );
+              batch.set(
+                eventDocRef(eventProviderId, eventId)
+                  .collection("privateUsers")
+                  .doc(privateId),
+                {
+                  id: privateId,
+                }
+              );
+            }
+          }
+          await batch.commit();
+          response.status(200).send({
+            type: "check_in",
+            status: "check_in_completed",
+          });
+          return;
         }
-        await batch.commit();
-        response.status(200).send({
-          type: "check_in",
-          status: "check_in_completed",
-        });
-        return;
-      }
+      });
     }
   );
 
