ab6789ce7f7fcb3acdf580cdcf74b7427608955d
Gian Marco Di Francesco <difrancescogianmarco@gmail.com>
2024-06-21 14:15:03 +0200

    migrate client functions to v2

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,21 +6,20 @@ const functionBaseUrl = kDebugMode && Environment.isEmulator
     ? 'http://localhost:5003/count-me-in-ef93b/europe-west3'
     : 'https://europe-west3-count-me-in-ef93b.cloudfunctions.net';
 const authority = kDebugMode && Environment.isEmulator ? 'localhost:5003' : 'cmi.digit.srl';
-const baseUrl = 'https://$authority';
-const passepartoutActivityId = 'wom-count-me-in';
 const womDomain = kDebugMode && Environment.isEmulator ? 'dev.wom.social': 'wom.social';
+const baseUrl = 'https://$authority';
 
 const countMeInProviderId = Environment.isEmulator ? 'Yy31B32YBDJDUt7TbZEl' : 'countmein';
 
-const createUserUrl = '$functionBaseUrl/userSignUp-createUser';
-const verifyOtpCodeUrl = '$functionBaseUrl/userProfile-verifyOtpCode';
-const recoverUserUrl = '$functionBaseUrl/userProfile-recoverUser';
-const groupCardUrl = '$functionBaseUrl/userSignUp-requestGroupCard';
-const recoverUserCardUrl = '$functionBaseUrl/userSignUp-recoverUserCard';
-const requestOtpCodeUrl = '$functionBaseUrl/userProfile-requestOtpCode';
-const verifyEmailUrl = '$functionBaseUrl/userSignUp-verifyEmail';
-const sendResetPasswordUrl = '$functionBaseUrl/userSignUp-sendResetPasswordEmail';
-const confirmPendingInviteUrl = '$functionBaseUrl/managers-confirmPendingInvite';
-const changePasswordUrl = '$functionBaseUrl/userSignUp-changePassword';
+const createUserUrl = '$functionBaseUrl/userSignUp-createUserSecondGen';
+const verifyOtpCodeUrl = '$functionBaseUrl/userProfile-verifyOtpCodeSecondGen';
+const recoverUserUrl = '$functionBaseUrl/userProfile-recoverUserSecondGen';
+const groupCardUrl = '$functionBaseUrl/userSignUp-requestGroupCardSecondGen';
+const recoverUserCardUrl = '$functionBaseUrl/userSignUp-recoverUserCardSecondGen';
+const requestOtpCodeUrl = '$functionBaseUrl/userProfile-requestOtpCodeSecondGen';
+const verifyEmailUrl = '$functionBaseUrl/userSignUp-verifyEmailSecondGen';
+const sendResetPasswordUrl = '$functionBaseUrl/userSignUp-sendResetPasswordEmailSecondGen';
+const confirmPendingInviteUrl = '$functionBaseUrl/managers-confirmPendingInviteSecondGen';
+const changePasswordUrl = '$functionBaseUrl/userSignUp-changePasswordSecondGen';
 
 const totemBaseUrl = 'https://link.wom.social/cmi';
